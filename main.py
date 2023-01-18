92f9b3f946e5b45a94112ef1346c7399613ec390
BogdanNovoselov <novoselov.b.d.2003@mail.ru>
2023-01-19 03:41:46 +0500

    Gjlrk.xtybt ,fp lfyys[

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/usersinformation'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(28), nullable=False, unique=True)
+    password = db.Column(db.String(28), nullable=False)
+
 
 @app.route('/')
-def index():
+def registration():
+    return render_template("registration.html")
+
+@app.route('/home')
+def home():
     return render_template("index.html")
 
 
